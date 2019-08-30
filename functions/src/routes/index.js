ad5342f2da6354aefacfc741b2b7b3a22118e1ca
daniellamarr <daniel.anyaegbu@andela.com>
2019-08-30 04:26:36 +0100

    apply to become a contributor

--- functions/src/routes/index.js
+++ functions/src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRoute = require('./users');
+const applicationRoute = require('./applications');
 
 const routes = express();
 
 routes.use('/users', userRoute);
+routes.use('/applications', applicationRoute);
 
 module.exports = routes;
