d8bcf1bb893606c1be66a9265246c5298fe4373a
timzprof <timilehin.olumofin@gmail.com>
2019-09-13 10:57:11 +0100

    Added Endpoints List

--- functions/src/routes/index.js
+++ functions/src/routes/index.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const userRoute = require('./users');
-const applicationRouter = require('./applications');
-const articlesRouter = require('./articles');
-const categoryRouter = require('./categories');
-const { configureCloudinary } = require('../helpers/cloudinary');
+const express = require("express");
+const listEndpoints = require("express-list-endpoints");
+const userRoute = require("./users");
+const applicationRouter = require("./applications");
+const articlesRouter = require("./articles");
+const categoryRouter = require("./categories");
+const {configureCloudinary} = require("../helpers/cloudinary");
 
 const routes = express();
 configureCloudinary();
 
-routes.use('/users', userRoute);
-routes.use('/applications', applicationRouter);
-routes.use('/articles', articlesRouter);
-routes.use('/categories', categoryRouter);
+routes.use("/users", userRoute);
+routes.use("/applications", applicationRouter);
+routes.use("/articles", articlesRouter);
+routes.use("/categories", categoryRouter);
+
+routes.use("/endpoints", (req, res) =>
+	res.status(200).json(listEndpoints(routes))
+);
 
 module.exports = routes;
