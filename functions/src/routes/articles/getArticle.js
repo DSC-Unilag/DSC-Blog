4f54c91482080f53a9e789124332b05f42097d97
timzprof <timilehin.olumofin@gmail.com>
2019-09-19 14:01:55 +0100

    Updated Firebase Deployment

--- functions/src/routes/articles/getArticle.js
+++ functions/src/routes/articles/getArticle.js
@@ -1,59 +1,49 @@
 /* eslint-disable promise/no-nesting */
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 
 const db = admin.firestore();
 
 const getArticle = (request, response) => {
-	const {id} = request.params;
-	return db
-		.collection("articles")
-		.doc(id)
-		.get()
-		.then(docRef => {
-			return {...docRef.data(), aid: docRef.id};
-		})
-		.then(data => {
-			return db
-				.collection("users")
-				.doc(data.user)
-				.get()
-				.then(docRef => {
-					const userData = docRef.data();
-					return {
-						...data,
-						user: {
-							firstname: userData.firstname,
-							lastname: userData.lastname,
-							email: userData.email,
-							role: userData.role,
-							uid: docRef.id
-						}
-					};
-				});
-		})
-		.then(data => {
-			return db
-				.collection("categories")
-				.doc(data.category)
-				.get()
-				.then(docRef => ({
-					...data,
-					category: {...docRef.data(), cid: docRef.id}
-				}));
-		})
-		.then(data => {
-			return response.status(200).json({
-				success: true,
-				message: "Article Retrieved",
-				data
-			});
-		})
-		.catch(() =>
-			response.status(500).send({
-				success: false,
-				message: "Something went wrong"
-			})
-		);
+  const { id } = request.params;
+  return db
+    .collection('articles')
+    .doc(id)
+    .get()
+    .then((docRef) => ({ ...docRef.data(), aid: docRef.id }))
+    .then((data) => db
+      .collection('users')
+      .doc(data.user)
+      .get()
+      .then((docRef) => {
+        const userData = docRef.data();
+        return {
+          ...data,
+          user: {
+            firstname: userData.firstname,
+            lastname: userData.lastname,
+            email: userData.email,
+            role: userData.role,
+            uid: docRef.id,
+          },
+        };
+      }))
+    .then((data) => db
+      .collection('categories')
+      .doc(data.category)
+      .get()
+      .then((docRef) => ({
+        ...data,
+        category: { ...docRef.data(), cid: docRef.id },
+      })))
+    .then((data) => response.status(200).json({
+      success: true,
+      message: 'Article Retrieved',
+      data,
+    }))
+    .catch(() => response.status(500).send({
+      success: false,
+      message: 'Something went wrong',
+    }));
 };
 
 module.exports = getArticle;
