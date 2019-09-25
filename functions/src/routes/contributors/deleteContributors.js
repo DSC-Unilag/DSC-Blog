8928fae3ac615a53e217885706fa6beaf52f6eb7
timzprof <timilehin.olumofin@gmail.com>
2019-09-25 13:33:16 +0100

    Linted Codebase

--- functions/src/routes/contributors/deleteContributors.js
+++ functions/src/routes/contributors/deleteContributors.js
@@ -1,26 +1,22 @@
 /* eslint-disable promise/no-nesting */
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 
 const db = admin.firestore();
 
 const deleteContributor = (request, response) => {
-	const {cid} = request.params;
-	return db
-		.collection("users")
-		.doc(cid)
-		.delete()
-		.then(() =>
-			response.status(200).send({
-				success: true,
-				message: "Contributor Deleted"
-			})
-		)
-		.catch(() =>
-			response.status(500).send({
-				success: false,
-				message: "Something went wrong"
-			})
-		);
+  const { cid } = request.params;
+  return db
+    .collection('users')
+    .doc(cid)
+    .delete()
+    .then(() => response.status(200).send({
+      success: true,
+      message: 'Contributor Deleted',
+    }))
+    .catch(() => response.status(500).send({
+      success: false,
+      message: 'Something went wrong',
+    }));
 };
 
 module.exports = deleteContributor;
