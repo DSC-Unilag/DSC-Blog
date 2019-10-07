aa9c5bcd42510f4c3436543e133160aa3e7e5e9d
timzprof <timilehin.olumofin@gmail.com>
2019-10-07 01:25:19 +0100

    Linted Codebase

--- functions/src/middleware/verifyToken.js
+++ functions/src/middleware/verifyToken.js
@@ -1,35 +1,34 @@
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 const verifyToken = (request, response, next) => {
-	let token =
-		request.headers.authorization ||
-		request.headers["x-access-token"] ||
-		request.query.token ||
-		request.body.token;
+  let token =		request.headers.authorization
+		|| request.headers['x-access-token']
+		|| request.query.token
+		|| request.body.token;
 
-	if (!token) {
-		return response.status(401).send({
-			success: false,
-			message: "You did not provide a token"
-		});
-	} else {
-		token = token.split(" ")[1];
-	}
+  if (!token) {
+    return response.status(401).send({
+      success: false,
+      message: 'You did not provide a token',
+    });
+  }
+  token = token.split(' ')[1];
 
-	return jwt.verify(token, process.env.TOKEN_SECRET, (err, payload) => {
-		if (err) {
-			return response.status(401).send({
-				success: false,
-				message: "Your token is invalid or expired"
-			});
-		}
 
-		request.payload = payload;
-		return next();
-	});
+  return jwt.verify(token, process.env.TOKEN_SECRET, (err, payload) => {
+    if (err) {
+      return response.status(401).send({
+        success: false,
+        message: 'Your token is invalid or expired',
+      });
+    }
+
+    request.payload = payload;
+    return next();
+  });
 };
 
 module.exports = verifyToken;
--- functions/src/routes/articles/editArticle.js
+++ functions/src/routes/articles/editArticle.js
@@ -1,114 +1,103 @@
 /* eslint-disable promise/no-nesting */
-const fs = require("fs");
-const admin = require("firebase-admin");
+const fs = require('fs');
+const admin = require('firebase-admin');
 const {
-	cloudinaryUpload,
-	cloudinaryDestroy,
-	getIdFromUrl
-} = require("../../helpers/cloudinary");
+  cloudinaryUpload,
+  cloudinaryDestroy,
+  getIdFromUrl,
+} = require('../../helpers/cloudinary');
 
 const db = admin.firestore();
 
 const handleEditArticle = (
-	request,
-	response,
-	articleRef,
-	{id, title, categoryId, content, imageUrl}
-) => {
-	return articleRef
-		.get()
-		.then(oldDoc => {
-			const doc = oldDoc.data();
-			if (doc.user !== request.payload.uid) {
-				return response.status(401).json({
-					success: false,
-					message: "You can only edit your articles"
-				});
-			}
-			return db
-				.collection("articles")
-				.doc(id)
-				.set(
-					{
-						title: title || doc.title,
-						content: content || doc.content,
-						category: categoryId || doc.category,
-						published: doc.published || false,
-						imageUrl: imageUrl || doc.imageUrl,
-						updated: new Date().getTime()
-					},
-					{merge: true}
-				);
-		})
-		.then(() =>
-			response.status(200).send({
-				success: true,
-				message: "Post Updated"
-			})
-		)
-		.catch(err => {
-			return response.status(500).send({
-				success: false,
-				message: "Article does not exist",
-				errM: err.message,
-				errS: err.stack
-			});
-		});
-};
+  request,
+  response,
+  articleRef,
+  {
+    id, title, categoryId, content, imageUrl,
+  },
+) => articleRef
+  .get()
+  .then((oldDoc) => {
+    const doc = oldDoc.data();
+    if (doc.user !== request.payload.uid) {
+      return response.status(401).json({
+        success: false,
+        message: 'You can only edit your articles',
+      });
+    }
+    return db
+      .collection('articles')
+      .doc(id)
+      .set(
+        {
+          title: title || doc.title,
+          content: content || doc.content,
+          category: categoryId || doc.category,
+          published: doc.published || false,
+          imageUrl: imageUrl || doc.imageUrl,
+          updated: new Date().getTime(),
+        },
+        { merge: true },
+      );
+  })
+  .then(() => response.status(200).send({
+    success: true,
+    message: 'Post Updated',
+  }))
+  .catch((err) => response.status(500).send({
+    success: false,
+    message: 'Article does not exist',
+    errM: err.message,
+    errS: err.stack,
+  }));
 
 const editArticle = (request, response) => {
-	const {id} = request.params;
-	const {title, content, categoryId} = request.body;
+  const { id } = request.params;
+  const { title, content, categoryId } = request.body;
 
-	const [image] = request.files;
-	let imageUrl = "";
-	const articleRef = db.collection("articles").doc(id);
-	if (image) {
-		return articleRef
-			.get()
-			.then(docRef => {
-				const doc = docRef.data();
-				if (doc.user !== request.payload.uid) {
-					return response.status(401).json({
-						success: false,
-						message: "You can only edit your articles"
-					});
-				}
-				const imageID = getIdFromUrl(doc.imageUrl);
-				return cloudinaryDestroy(imageID);
-			})
-			.then(() => {
-				return cloudinaryUpload(image.filepath);
-			})
-			.then(result => {
-				fs.unlinkSync(image.filepath, error => {
-					if (error) throw new Error(error.message);
-				});
-				imageUrl = result.secure_url;
-			})
-			.catch(err => {
-				return response.status(500).send({
-					success: false,
-					message: "Something went wrong while uploading image"
-				});
-			})
-			.finally(() => {
-				return handleEditArticle(request, response, articleRef, {
-					id,
-					title,
-					content,
-					categoryId,
-					imageUrl
-				});
-			});
-	} else {
-		return handleEditArticle(request, response, articleRef, {
-			id,
-			title,
-			content,
-			categoryId,
-			imageUrl
-		});
-	}
+  const [image] = request.files;
+  let imageUrl = '';
+  const articleRef = db.collection('articles').doc(id);
+  if (image) {
+    return articleRef
+      .get()
+      .then((docRef) => {
+        const doc = docRef.data();
+        if (doc.user !== request.payload.uid) {
+          return response.status(401).json({
+            success: false,
+            message: 'You can only edit your articles',
+          });
+        }
+        const imageID = getIdFromUrl(doc.imageUrl);
+        return cloudinaryDestroy(imageID);
+      })
+      .then(() => cloudinaryUpload(image.filepath))
+      .then((result) => {
+        fs.unlinkSync(image.filepath, (error) => {
+          if (error) throw new Error(error.message);
+        });
+        imageUrl = result.secure_url;
+      })
+      .catch(() => response.status(500).send({
+        success: false,
+        message: 'Something went wrong while uploading image',
+      }))
+      .finally(() => handleEditArticle(request, response, articleRef, {
+        id,
+        title,
+        content,
+        categoryId,
+        imageUrl,
+      }));
+  }
+  return handleEditArticle(request, response, articleRef, {
+    id,
+    title,
+    content,
+    categoryId,
+    imageUrl,
+  });
 };
 module.exports = editArticle;
