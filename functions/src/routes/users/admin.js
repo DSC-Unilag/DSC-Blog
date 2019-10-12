e39155971b16030af59d59026d29926d65b0d9fa
timzprof <timilehin.olumofin@gmail.com>
2019-10-12 22:46:52 +0100

    Linted Codebase

--- functions/src/routes/users/admin.js
+++ functions/src/routes/users/admin.js
@@ -1,82 +1,76 @@
 /* eslint-disable promise/no-nesting */
-const admin = require("firebase-admin");
-const randomize = require("../../helpers/randomize");
-const passwordHash = require("../../helpers/passwordHash");
-const verifyUser = require("../../helpers/verifyUser");
-const userPermissions = require("../../helpers/userPermissions");
-const {sendMail} = require("../../helpers/mail");
+const admin = require('firebase-admin');
+const randomize = require('../../helpers/randomize');
+const passwordHash = require('../../helpers/passwordHash');
+const verifyUser = require('../../helpers/verifyUser');
+const userPermissions = require('../../helpers/userPermissions');
+const { sendMail } = require('../../helpers/mail');
 
 const db = admin.firestore();
 
 const createAdmin = (request, response) => {
-	const {firstname, lastname, email} = request.body;
-	if (!firstname || !lastname || !email) {
-		return response.status(400).send({
-			success: false,
-			message: "Missing input/fields"
-		});
-	}
-	let actualPassword;
-	return verifyUser({email}, ["email"]).then(check => {
-		if (check) {
-			return response.status(409).send({
-				success: false,
-				message: "This user already exists"
-			});
-		}
-		actualPassword = randomize(10);
-		const password = passwordHash.hash(actualPassword, 10);
-		const role = "admin";
-		const claims = userPermissions(role);
-		if (!claims) {
-			return response.status(409).send({
-				success: false,
-				message: "Please add a valid role"
-			});
-		}
+  const { firstname, lastname, email } = request.body;
+  if (!firstname || !lastname || !email) {
+    return response.status(400).send({
+      success: false,
+      message: 'Missing input/fields',
+    });
+  }
+  let actualPassword;
+  return verifyUser({ email }, ['email']).then((check) => {
+    if (check) {
+      return response.status(409).send({
+        success: false,
+        message: 'This user already exists',
+      });
+    }
+    actualPassword = randomize(10);
+    const password = passwordHash.hash(actualPassword, 10);
+    const role = 'admin';
+    const claims = userPermissions(role);
+    if (!claims) {
+      return response.status(409).send({
+        success: false,
+        message: 'Please add a valid role',
+      });
+    }
 
-		return db
-			.collection("users")
-			.add({
-				firstname,
-				lastname,
-				email,
-				password,
-				role,
-				claims,
-				updated: new Date().getTime(),
-				created: new Date().getTime()
-			})
-			.then(() => {
-				return sendMail({
-					to: email,
-					subject: "Congratulations Admin!",
-					html: `<p>You have been selected to be an administrator on DSC Unilag Blog</p>
+    return db
+      .collection('users')
+      .add({
+        firstname,
+        lastname,
+        email,
+        password,
+        role,
+        claims,
+        updated: new Date().getTime(),
+        created: new Date().getTime(),
+      })
+      .then(() => sendMail({
+        to: email,
+        subject: 'Congratulations Admin!',
+        html: `<p>You have been selected to be an administrator on DSC Unilag Blog</p>
                 <p>These are your login details</p>
                 <p>Email: ${email}</p>
-                <p>Password: ${actualPassword}</p>`
-				});
-			})
-			.then(() => {
-				return response.status(201).send({
-					success: true,
-					message: "Admin successfully created",
-					data: {
-						firstname,
-						lastname,
-						email,
-						role,
-						claims
-					}
-				});
-			})
-			.catch(() =>
-				response.status(500).send({
-					success: false,
-					message: "Something went wrong"
-				})
-			);
-	});
+                <p>Password: ${actualPassword}</p>`,
+      }))
+      .then(() => response.status(201).send({
+        success: true,
+        message: 'Admin successfully created',
+        data: {
+          firstname,
+          lastname,
+          email,
+          role,
+          claims,
+        },
+      }))
+      .catch(() => response.status(500).send({
+        success: false,
+        message: 'Something went wrong',
+      }));
+  });
 };
 
-module.exports = {createAdmin};
+module.exports = { createAdmin };
--- functions/src/routes/users/contributor.js
+++ functions/src/routes/users/contributor.js
@@ -1,88 +1,84 @@
 /* eslint-disable promise/no-nesting */
-const admin = require("firebase-admin");
-const randomize = require("../../helpers/randomize");
-const passwordHash = require("../../helpers/passwordHash");
-const userPermissions = require("../../helpers/userPermissions");
-const {sendMail} = require("../../helpers/mail");
+const admin = require('firebase-admin');
+const randomize = require('../../helpers/randomize');
+const passwordHash = require('../../helpers/passwordHash');
+const userPermissions = require('../../helpers/userPermissions');
+const { sendMail } = require('../../helpers/mail');
 
 const db = admin.firestore();
 
 const createContributor = (request, response) => {
-	const {conid} = request.body;
-	if (!conid) {
-		return response.status(400).send({
-			success: false,
-			message: "Missing input/fields"
-		});
-	}
-	let actualPassword;
-	return db
-		.collection("applications")
-		.doc(conid)
-		.get()
-		.then(docRef => {
-			const applicantData = docRef.data();
-			if (applicantData.status !== "reviewed") {
-				return response.status(412).send({
-					success: false,
-					message: "Review Application before approval"
-				});
-			}
-			actualPassword = randomize(10);
-			const password = passwordHash.hash(actualPassword, 10);
-			const role = "contributor";
-			const claims = userPermissions(role);
-			if (!claims) {
-				return response.status(409).send({
-					success: false,
-					message: "Please add a valid role"
-				});
-			}
+  const { conid } = request.body;
+  if (!conid) {
+    return response.status(400).send({
+      success: false,
+      message: 'Missing input/fields',
+    });
+  }
+  let actualPassword;
+  return db
+    .collection('applications')
+    .doc(conid)
+    .get()
+    .then((docRef) => {
+      const applicantData = docRef.data();
+      if (applicantData.status !== 'reviewed') {
+        return response.status(412).send({
+          success: false,
+          message: 'Review Application before approval',
+        });
+      }
+      actualPassword = randomize(10);
+      const password = passwordHash.hash(actualPassword, 10);
+      const role = 'contributor';
+      const claims = userPermissions(role);
+      if (!claims) {
+        return response.status(409).send({
+          success: false,
+          message: 'Please add a valid role',
+        });
+      }
 
-			return db
-				.collection("users")
-				.add({
-					firstname: applicantData.firstname,
-					lastname: applicantData.lastname,
-					email: applicantData.email,
-					password,
-					role,
-					claims,
-					updated: new Date().getTime(),
-					created: new Date().getTime()
-				})
-				.then(() => {
-					sendMail({
-						to: applicantData.email,
-						subject: "Application Approved",
-						html: `<p>Your application to write for DSC Blog has been approved</p>
+      return db
+        .collection('users')
+        .add({
+          firstname: applicantData.firstname,
+          lastname: applicantData.lastname,
+          email: applicantData.email,
+          password,
+          role,
+          claims,
+          updated: new Date().getTime(),
+          created: new Date().getTime(),
+        })
+        .then(() => {
+          sendMail({
+            to: applicantData.email,
+            subject: 'Application Approved',
+            html: `<p>Your application to write for DSC Blog has been approved</p>
                   <p>These are your login details</p>
                   <p>Email: ${applicantData.email}</p>
-                  <p>Password: ${actualPassword}</p>`
-					});
-				})
-				.then(() =>
-					response.status(201).send({
-						success: true,
-						message: "Contributor successfully created",
-						data: {
-							firstname: applicantData.firstname,
-							lastname: applicantData.lastname,
-							email: applicantData.email,
-							role
-						}
-					})
-				);
-		})
-		.catch(err =>
-			response.status(500).send({
-				success: false,
-				message: "Something went wrong",
-				errM: err.message,
-				errS: err.stack,
-				err
-			})
-		);
+                  <p>Password: ${actualPassword}</p>`,
+          });
+        })
+        .then(() => response.status(201).send({
+          success: true,
+          message: 'Contributor successfully created',
+          data: {
+            firstname: applicantData.firstname,
+            lastname: applicantData.lastname,
+            email: applicantData.email,
+            role,
+          },
+        }));
+    })
+    .catch((err) => response.status(500).send({
+      success: false,
+      message: 'Something went wrong',
+      errM: err.message,
+      errS: err.stack,
+      err,
+    }));
 };
 
-module.exports = {createContributor};
+module.exports = { createContributor };
