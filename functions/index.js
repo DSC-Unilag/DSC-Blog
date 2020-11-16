8a3c3ba57855e21be4e5c6a8350f3bb127bb3dda
emmanuerl <emekaemmanuel045@gmail.com>
2020-11-16 20:43:34 +0100

    initial commit

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,27 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const firebase = require("firebase");
-const config = require("./config/config.json");
-const serviceAccount = require("./config/serviceAccountKey.json");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const firebase = require('firebase');
+const config = require('./config/config.json');
+const serviceAccount = require('./config/serviceAccountKey.json');
 
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: config.databaseURL
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: config.databaseURL,
 });
 
 firebase.initializeApp({
-	apiKey: config.apiKey,
-	authDomain: config.authDomain,
-	databaseURL: config.databaseURL,
-	storageBucket: config.storageBucket
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  databaseURL: config.databaseURL,
+  storageBucket: config.storageBucket,
 });
 
-const routes = require("./src/routes");
+const routes = require('./src/routes');
 
-module.exports.welcome = functions.https.onRequest((request, response) => {
-	return response.status(200).send({
-		success: true,
-		message: "Welcome to DSC Blog Firebase Functions"
-	});
-});
+module.exports.welcome = functions.https.onRequest((request, response) => response.status(200)
+  .send({
+    success: true,
+    message: 'Welcome to DSC Blog Firebase Functions',
+  }));
 
 module.exports.app = functions.https.onRequest(routes);
